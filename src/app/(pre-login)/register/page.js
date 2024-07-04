d8be8f5c810dff52c186ddd6260a7416fd8029aa
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-07-03 20:29:42 -0400

    Logical navigation

--- src/app/(pre-login)/register/page.js
+++ src/app/(pre-login)/register/page.js
@@ -1,5 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation"
+
 export default function RegisterUI() {
+    const router = useRouter();
+    const navigate = () => {
+        // router.push('/login');
+        // router.back();
+        // router.forward();
+        router.replace('/login');
+    }
     return (
-        <h1>Register component</h1>
+        <div>
+            <h1>Register component</h1>
+            <button onClick={navigate}>Register and Navigate</button>
+        </div>
     )
 }
