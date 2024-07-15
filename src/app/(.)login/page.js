6de450a648f43c3525f1fd945e32495aa730a233
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-07-15 19:27:01 -0400

    Intercepted routes

---
+++ src/app/(.)login/page.js
@@ -0,0 +1,15 @@
+export default function InterceptedLogin() {
+    return (
+        <div className="modal-content">
+            <div className="modal-header">
+                <h1>Header</h1>
+            </div>
+            <div className="modal-body">
+                <h1>Body</h1>
+            </div>
+            <div className="modal-footer">
+                <h1>Footer</h1>
+            </div>
+        </div>
+    )
+}
