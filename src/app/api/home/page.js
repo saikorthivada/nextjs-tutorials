30a24d06fd3ce1472caa49be838a7b50971a81ee
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-07-15 21:29:50 -0400

    API removed

--- src/app/api/home/page.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <h1>Hai this is Home</h1>
-    )
-}
--- src/app/api/home/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET() {
-    return new Response(JSON.stringify({
-        name: 'techshareskk'
-    }));
-}
