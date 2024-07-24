791603ce88dbbd97f1bc1ecd833c580855b44048
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-07-24 19:35:49 -0400

    API Headers

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,25 +1,18 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Users } from "./user.meta";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-    return new Response(JSON.stringify(Users), {
-        headers: {
-            "content-type":"application/json"
-        },
+export async function GET(req: NextRequest) {
+    // console.log(req.headers.get('authorization'));
+
+    // console.log(headers());
+    const data = {
+        sample: 'sai'
+    }
+    return new Response(JSON.stringify(data), {
         status: 200,
-        statusText: 'Successfully fetched'
-    });
-}
-
-export async function POST(req: NextRequest, response: NextResponse) {
-    const result = await req.json();
-    console.log('Im ready', result);
-    Users.push(result);
-    return new Response(JSON.stringify(result), {
         headers: {
-            "content-type": "application/json"
-        },
-        status: 201,
-        statusText: 'Created Successfully'
+            "content-type": "text/plain",
+            "data": 'kahhjsgahjgsjahgs'
+        }
     })
 }
