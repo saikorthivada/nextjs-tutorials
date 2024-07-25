4db3f792524b0e15ce00773c9e532e195613c8e4
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-07-24 20:03:56 -0400

    Cookies

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,18 +1,10 @@
-import { headers } from "next/headers";
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    // console.log(req.headers.get('authorization'));
+    // console.log(req.cookies.clear());
+    console.log(cookies().getAll());
 
-    // console.log(headers());
-    const data = {
-        sample: 'sai'
-    }
-    return new Response(JSON.stringify(data), {
-        status: 200,
-        headers: {
-            "content-type": "text/plain",
-            "data": 'kahhjsgahjgsjahgs'
-        }
-    })
+    cookies().set("pageSize", "20");
+    return new Response('Hai this is cookie session')
 }
